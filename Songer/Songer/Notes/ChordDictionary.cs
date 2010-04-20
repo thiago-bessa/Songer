9303df0ce042f7467a1b62bd519fbfb7376ae94b
Thiago Bessa <thiago@bessa.net.br>
2010-04-20 01:48:19 +0000

    Initial upload of files

---
+++ Songer/Songer/Notes/ChordDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Songer
+{
+    public class ChordDictionary : List<Chord>
+    {
+        public ChordDictionary(string filename, MusicalNoteDictionary notes)
+        {
+            StreamReader reader = new StreamReader(filename);
+
+            while (!reader.EndOfStream)
+            {
+                Chord chord = Chord.FromString(reader.ReadLine(), notes);
+                this.Add(chord);
+            }
+        }
+    }
+}
