b5eb6adf54cc36b38fac6e4c799a7565a1e33b02
kbessa <kbessa@f18a227e-ac65-45ea-bea5-086cd7eced54>
2010-04-23 02:10:40 +0000

    SoundInput finalized

--- trunk/Songer/Songer.SoundInput/WaveFileCapture.cs
+++ trunk/Songer/Songer.SoundInput/WaveFileCapture.cs
@@ -8,7 +8,7 @@ using SlimDX.Multimedia;
 
 namespace Songer.SoundInput
 {
-    public class WaveFileCapture : SoundSource
+    public class WaveFileCapture : SoundSource, IDisposable
     {
         private WaveStream waveStream;
         private Thread processingThread;
@@ -21,32 +21,31 @@ namespace Songer.SoundInput
             this.Format = this.waveStream.Format;
         }
         
-        public override void Listen()
+        public override void Start()
         {
-            this.processingThread = new Thread(new ThreadStart(this.ProcessWaveFile));
+            this.processingThread = new Thread(new ThreadStart(this.CaptureLoop));
             this.processingThread.Name = String.Format("Processing WaveFile: {0}", this.filename);
             this.processingThread.Start();
         }
 
-        public void ProcessWaveFile()
+        protected override void CaptureLoop()
         {
 
         getHere:
 
             int bufferSize = this.Format.AverageBytesPerSecond;
-            byte[] data = new byte[bufferSize];
+            byte[] waveData = new byte[bufferSize];
             int position = 0;
-
-
+            
             while (position < this.waveStream.Length)
             {
-                position += this.waveStream.Read(data, 0, bufferSize);
+                position += this.waveStream.Read(waveData, 0, bufferSize);
 
                 //Convert byte[] data to short[]
-                short[] x = new short[bufferSize / 2];
-                Buffer.BlockCopy(data, 0, x, 0, bufferSize);
+                short[] soundData = new short[bufferSize / 2];
+                Buffer.BlockCopy(waveData, 0, soundData, 0, bufferSize);
 
-                this.ProcessData(x);
+                this.OnSoundDetected(soundData);
             }
 
             this.waveStream.Position = 0;
@@ -60,7 +59,19 @@ namespace Songer.SoundInput
             if (this.processingThread.IsAlive)
             {
                 this.processingThread.Abort();
+                this.waveStream.Close();
             }
         }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            this.Stop();
+            this.waveStream.Dispose();
+        }
+
+        #endregion
     }
 }
