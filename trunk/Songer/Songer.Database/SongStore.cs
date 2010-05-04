0f372b56b51f29be0556d519b1f3536914ad6393
kbessa <kbessa@f18a227e-ac65-45ea-bea5-086cd7eced54>
2010-05-04 01:19:10 +0000

    Songer.Database and Songer.Presentation added

---
+++ trunk/Songer/Songer.Database/SongStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Db4objects.Db4o;
+
+namespace Songer.Database
+{
+    public class SongStore
+    {
+        private IObjectContainer database;
+
+        public SongStore()
+        {
+            this.database = Db4oEmbedded.OpenFile("SongStore.yap");
+        }
+
+        public void Add(Song song)
+        {
+            this.database.Store(song);
+            this.database.Commit();
+        }
+
+        public void Delete(Song song)
+        {
+            this.database.Delete(song);
+            this.database.Commit();
+        }
+
+        public IList<Song> Search(string chordSequence)
+        {
+            return this.database.Query<Song>(s => s.Matches(chordSequence));
+        }
+    }
+}
